1c56e534b01b613ed4e9e71cd9f76bf64448f1ea
Kiwon Kim <kiwon@elicer.com>
2023-07-20 23:01:17 +0900

    feat: setup eas test build with expo for android

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import WebView from "react-native-webview";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <WebView
+        style={styles.container}
+        source={{ uri: "https://staywith.kr" }}
+      />
     </View>
   );
 }
@@ -13,8 +17,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 ## Install Process
 
 [Use TypeScript](https://docs.expo.dev/guides/typescript/)
+
+## Webview
+
+[React Native WebView Guide](https://github.com/react-native-webview/react-native-webview/blob/master/docs/Guide.md#react-native-webview-guide)
+
+## EAS Build
+
+### App Store에 배포
+
+https://docs.expo.dev/build/setup/#build-for-app-stores
+
+### 테스트 빌드
+
+#### 안드로이드
+
+eas.json에 있는 build.preview 옵션에 따라 expo에 build
+
+`eas build -p android --profile preview` 또는 `yarn build:preview`
+
+https://docs.expo.dev/build-reference/apk/
